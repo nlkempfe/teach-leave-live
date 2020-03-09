85f9828a0b3464e956dc8d2459fd10702d0883ab
Michael Hoffman <mdhoffman271@gmail.com>
2020-03-09 19:07:23 -0400

    Basic NavBar, needs work

--- teach-leave-live/client/src/components/NavigationBar.js
+++ teach-leave-live/client/src/components/NavigationBar.js
@@ -1,33 +1,33 @@
 import React, { useState } from 'react';
 
 /* Import assets */
-import PersonFillIcon from '../assets/icons/person-fill.svg';
+import LogoLight from '../assets/logo_light.png'
+import BookMark from '../assets/icons/bookmark.svg'
 
 /* Import bootstrap components */
-import Image from 'react-bootstrap/Image';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Image from 'react-bootstrap/Image'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
 
 function NavigationBar () {
-  return (
-    <div>
-      <Navbar bg = "dark" fixed = "top" variant = "dark">
-        <Navbar.Brand>
-          Teach Leave Live
-        </Navbar.Brand>
-        <Navbar.Toggle/>
-        <Navbar.Collapse className = "justify-content-end" variant = "dark">
-            <Nav.Link>Home</Nav.Link>
-            <Nav.Link>Blog</Nav.Link>
-            <Nav.Link>Courses</Nav.Link>
-            <Nav.Link>
-              <Image src = {PersonFillIcon}/>
-              {' Account'}
-            </Nav.Link>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
-  );
+    return (
+        <div>
+            <Navbar bg = "light" fixed = "top" variant = "light">
+                <Navbar.Collapse className = "justify-content-between">
+                    <Image src = {LogoLight} fluid />
+                    <Form>
+                        <FormControl type="text" placeholder="Search" className="mr-auto" />
+                    </Form>
+                    <Nav.Link className = "text-dark">Sign up</Nav.Link>
+                    <Nav.Link className = "text-dark">Login</Nav.Link>
+                    <Nav.Link className = "text-dark">Classroom</Nav.Link>
+                    <Image src = {BookMark} fluid/>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    );
 }
 
 export default NavigationBar;
