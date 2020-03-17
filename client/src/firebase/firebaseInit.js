766fa2c0c46a60f2dc4f12dcda7ae6cde00f1fd4
Natascha Kempfe <nlkempfe@gmail.com>
2020-03-17 17:37:14 -0400

    Remove unnecessary files, update process environment variables

--- client/src/firebase/firebaseInit.js
+++ client/src/firebase/firebaseInit.js
@@ -1,12 +1,23 @@
-//Import Firebase app
+/* Import Firebase app */
 import firebase from "firebase";
 
-//Import private config structure for initialization
-import config from "../firebaseConfig";
+/* Import environment variables for initialization */
+import config from '../firebaseConfig.js';
 
-firebase.initializeApp(config);
+const credentials = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || config.apiKey,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || config.authDomain,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL || config.databaseURL,
+  projectId: 'teach-leave-live',
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || config.storageBucket,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || config.messagingSenderId,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID || config.appId,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || config.measurementId
+};
 
-//Exports
+firebase.initializeApp(credentials);
+
+/* Exports */
 export const auth = firebase.auth;
 export const provider = new firebase.auth.FacebookAuthProvider();
 export const db = firebase.firestore();
