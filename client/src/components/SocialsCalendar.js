31036d8aa2aee0b44eb946c8217577afbba3766b
Michael Hoffman <mdhoffman271@gmail.com>
2020-03-22 15:48:00 -0400

    Added react-calendar

---
+++ client/src/components/SocialsCalendar.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
+
+
+function SocialsCalendar(props){
+  return <Calendar />
+}
+
+export default SocialsCalendar;
