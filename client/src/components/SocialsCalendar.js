73b827f984b652adff52f1a036a2c6fcd014d477
Michael Hoffman <mdhoffman271@gmail.com>
2020-03-27 14:21:31 -0400

    Adding a card for viewing events in a day

--- client/src/components/SocialsCalendar.js
+++ client/src/components/SocialsCalendar.js
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
+
 import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css';
+
+import Card from '@material-ui/core/card'
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 
 
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
@@ -17,9 +24,36 @@ function getTileContent(tiles){
   }
 }
 
+const dateToString = (date) => date.getFullYear() + date.getMonth() + date.getDate();
+
+const dateTimeFormat = new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' })
+
 
 function SocialsCalendar(props){
-  return <Calendar tileContent = {props.tiles ? getTileContent(props.tiles) : null} />
+  const [clickedDay, setClickedDay] = useState(null);
+  const onClickDay = (value, event) => setClickedDay(value);
+  const card = clickedDay == null ? null : (
+    <Card>
+      <CardContent>
+        <Typography color="textSecondary">
+          {dateTimeFormat.format(clickedDay)}
+        </Typography>
+      </CardContent>
+    </Card>
+  )
+
+  return (
+    <Container>
+      <Grid container spacing = {5}>
+        <Grid item>
+          <Calendar tileContent = {props.tiles ? getTileContent(props.tiles) : null} onClickDay = {onClickDay}/>
+        </Grid>
+        <Grid item>
+          {card}
+        </Grid>
+      </Grid>
+    </Container>
+  )
 }
 
 export default SocialsCalendar;
