fb9ce0a6676279e0952d53fda418e221aabecc07
rieckn <35986321+rieckn@users.noreply.github.com>
2020-03-30 17:05:00 -0400

    Finished profile button
    I finished the button that's name is the name that corresponds to the user ID that is passed into the component. When the button is pressed a popup appears with the users profile information.

--- client/src/components/ProfileLink.js
+++ client/src/components/ProfileLink.js
@@ -9,16 +9,25 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import Slide from '@material-ui/core/Slide';
+import Avatar from '@material-ui/core/Avatar';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
 
 /* Import firebase products */
 import {auth, provider, db} from "../firebase/firebaseInit";
   
-  function ProfileLink(props) {
+function ProfileLink(props) {
+    //Hold all information about a user
     const [open, setOpen] = useState(false);
-
-    let userName = ";"
+    const [buttonName,setButtonName] = useState('')
+    const [pictureURL,setPictureURL] = useState('')
+    const [email,setEmail] = useState('')
+    const [role,setRole] = useState('')
+    const [membership,setMembership] = useState(false)
   
+    //Handles opening and closing of the Dialog object
     const handleClickOpen = () => {
       setOpen(true);
     };
@@ -27,25 +36,80 @@ import {auth, provider, db} from "../firebase/firebaseInit";
       setOpen(false);
     };
 
-    if (props != undefined)
-    {
-        userName = props.name.toString()
-    }
+    //Checks the role of the user and returns it to the Dialog object to be displayed
+    const checkRole = () => {
+      if ((role.localeCompare('user')) === 0)
+      {
+        return (checkPremium());
+      }
+      else
+      {
+       return ("Admin")
+      }
+    };
+
+    //Returns email of user and nothing if they don't havee a registered email
+    const checkEmail = () => {
+      if ((email.localeCompare('N/A')) === 0)
+      {
+        return (null)
+      }
+      else
+      {
+       return ("Email: " + email)
+      }
+    };
+
+    //Returns the membership of the user
+    const checkPremium = () => {
+      if (membership)
+      {
+        return ("Premium Member")
+      }
+      else
+      {
+       return ("Standard Member")
+      }
+    };
+
+    //Get information of user from passed in UID and store it in respective variables
+    let userRef = db.collection('users').doc(props.uid);
+    let getDoc = userRef.get()
+    .then(doc => {
+      if (!doc.exists)
+      {
+        console.log('No Such Document')
+      }
+      else {
+        setButtonName(doc.data().firstName + ' ' + doc.data().lastName);
+        setPictureURL(doc.data().picURL)
+        setEmail(doc.data().email)
+        setRole(doc.data().role)
+        setMembership(doc.data().premium)
+      }
+    })
 
     return (
       <div>
-      <Button onClick={handleClickOpen}>{userName}</Button>
+      <Button onClick={handleClickOpen}>{console.log(buttonName)}{buttonName}</Button>
         <Dialog
           open={open}
           onClose={handleClose}
           aria-labelledby="alert-dialog-slide-title"
           aria-describedby="alert-dialog-slide-description"
         >
-          <DialogTitle id="alert-dialog-slide-title">{userName}</DialogTitle>
+          <DialogTitle id="alert-dialog-slide-title">
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar alt={buttonName} src={pictureURL}></Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={buttonName}></ListItemText>
+            </ListItem>
+          </DialogTitle>
           <DialogContent>
             <DialogContentText id="alert-dialog-slide-description">
-              Let Google help apps determine location. This means sending anonymous location data to
-              Google, even when no apps are running.
+              {checkRole()}<br/>
+              {checkEmail()}
             </DialogContentText>
           </DialogContent>
           <DialogActions>
--- client/src/views/Blog.js
+++ client/src/views/Blog.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import ProfileLink from '../components/ProfileLink.js'
 
 function Blog() {
-  let userName = "Noah Rieck"
+  let userID = 'a2c60dovnNajfSG91rnB4UXmBlf2'
   return (
     <div>
       <p>
@@ -12,7 +12,7 @@ function Blog() {
       <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dolor nibh, fermentum porttitor pulvinar sed, lacinia a lorem. Sed iaculis augue vel dictum rhoncus. Suspendisse venenatis, tellus nec sollicitudin molestie, urna felis semper mi, vitae laoreet nulla mi rhoncus risus. Ut dictum mauris nec ornare fermentum. Donec vulputate felis in vestibulum fermentum. Etiam varius varius nunc sed pharetra. In in est libero. Aenean auctor at ante eu scelerisque. Vestibulum vitae convallis mauris. Nam risus nibh, pellentesque a lacus eget, accumsan ultricies arcu. Phasellus finibus risus a velit dapibus, eget laoreet erat maximus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris varius volutpat luctus. Integer tincidunt feugiat justo. Phasellus eu sem urna. Pellentesque eget vehicula enim.
       </p>
-      <div><ProfileLink name={userName}/></div>
+      <div><ProfileLink uid={userID}/></div>
     </div>
   );
 }
