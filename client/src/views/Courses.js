5ee9076f4639dd7c129ec6f79574ec74bb8ec696
Michael Hoffman <mdhoffman271@gmail.com>
2020-03-29 21:33:52 -0400

    Fixed spelling

--- client/src/views/Courses.js
+++ client/src/views/Courses.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import EmbededVideo from '../components/EmbededVideo.js'
+import EmbeddedVideo from '../components/EmbeddedVideo.js'
 
 
 function Courses(props) {
@@ -16,7 +16,7 @@ function Courses(props) {
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dolor nibh, fermentum porttitor pulvinar sed, lacinia a lorem. Sed iaculis augue vel dictum rhoncus. Suspendisse venenatis, tellus nec sollicitudin molestie, urna felis semper mi, vitae laoreet nulla mi rhoncus risus. Ut dictum mauris nec ornare fermentum. Donec vulputate felis in vestibulum fermentum. Etiam varius varius nunc sed pharetra. In in est libero. Aenean auctor at ante eu scelerisque. Vestibulum vitae convallis mauris. Nam risus nibh, pellentesque a lacus eget, accumsan ultricies arcu. Phasellus finibus risus a velit dapibus, eget laoreet erat maximus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris varius volutpat luctus. Integer tincidunt feugiat justo. Phasellus eu sem urna. Pellentesque eget vehicula enim.
       </p>
       <div style = {{width: '50%', marginLeft: 'auto', marginRight: 'auto'}}>
-        <EmbededVideo youtubeId='cNr3r3nT8KE' />
+        <EmbeddedVideo youtubeId='cNr3r3nT8KE' />
       </div>
     </div>
   );
