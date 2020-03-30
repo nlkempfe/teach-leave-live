7541e23654a187e05c96c6c6ed5f6928bcafed0f
Michael Hoffman <mdhoffman271@gmail.com>
2020-03-29 21:44:50 -0400

    Moved inline styling

--- client/src/views/Courses.js
+++ client/src/views/Courses.js
@@ -6,7 +6,13 @@ import EmbeddedVideo from '../components/EmbeddedVideo.js'
 function Courses(props) {
 
 
-  /*Contains an example embeded video*/
+  const simple_video_style = {
+    width: '50%',
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  }
+
+
   return (
     <div>
       <p>
@@ -15,7 +21,7 @@ function Courses(props) {
       <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dolor nibh, fermentum porttitor pulvinar sed, lacinia a lorem. Sed iaculis augue vel dictum rhoncus. Suspendisse venenatis, tellus nec sollicitudin molestie, urna felis semper mi, vitae laoreet nulla mi rhoncus risus. Ut dictum mauris nec ornare fermentum. Donec vulputate felis in vestibulum fermentum. Etiam varius varius nunc sed pharetra. In in est libero. Aenean auctor at ante eu scelerisque. Vestibulum vitae convallis mauris. Nam risus nibh, pellentesque a lacus eget, accumsan ultricies arcu. Phasellus finibus risus a velit dapibus, eget laoreet erat maximus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris varius volutpat luctus. Integer tincidunt feugiat justo. Phasellus eu sem urna. Pellentesque eget vehicula enim.
       </p>
-      <EmbeddedVideo youtubeId='cNr3r3nT8KE' style = {{width: '50%', marginLeft: 'auto', marginRight: 'auto'}}/>
+      <EmbeddedVideo youtubeId='cNr3r3nT8KE' style = {simple_video_style}/>
     </div>
   );
 }
