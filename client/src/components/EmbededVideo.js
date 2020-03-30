c300c49752c724ef682848671edfcf5450ab4bfb
Michael Hoffman <mdhoffman271@gmail.com>
2020-03-29 21:30:25 -0400

    Embeded video is made dynamic with fixed aspect ratio

--- client/src/components/EmbededVideo.js
+++ client/src/components/EmbededVideo.js
@@ -1,11 +1,35 @@
 import React, { useState } from 'react';
 
 
-// TODO make the height and width scalable for mobile users
+/*
+Provides an embeded youtube video that fills its container and matches an aspect ratio
+Provide props.aspectRatio to specify the aspect ratio (e.g. 16.0/9.0)
+Default aspect ratio is 16.0/9.0 if props.aspect ratio is not provided
+*/
 function EmbededVideo (props) {
 
+  /*Calculate inverse of aspect ratio and convert into a percentage*/
+  const inverseAspectRatio = ((props.aspectRatio ? 1.0/props.aspectRatio : 9.0/16.0) * 100) + '%'
+
+  const container_style = {
+    position: 'relative',
+    overflow: 'hidden',
+    paddingTop: inverseAspectRatio
+  };
+
+  const iframe_style = {
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    width: '100%',
+    height: '100%',
+    border: '0'
+  };
+
   return (
-    <iframe width='560' height='315' src={'https://www.youtube.com/embed/' + props.youtubeId} frameborder='0' allow='accelerometer; encrypted-media; gyroscope; picture-in-picture' allowfullscreen />
+    <div style = {container_style}>
+      <iframe style = {iframe_style} src={'https://www.youtube.com/embed/' + props.youtubeId} frameborder='0' allowfullscreen />
+    </div>
   );
 }
 
--- client/src/views/Courses.js
+++ client/src/views/Courses.js
@@ -4,6 +4,9 @@ import EmbededVideo from '../components/EmbededVideo.js'
 
 
 function Courses(props) {
+
+
+  /*Contains an example embeded video*/
   return (
     <div>
       <p>
@@ -12,7 +15,9 @@ function Courses(props) {
       <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dolor nibh, fermentum porttitor pulvinar sed, lacinia a lorem. Sed iaculis augue vel dictum rhoncus. Suspendisse venenatis, tellus nec sollicitudin molestie, urna felis semper mi, vitae laoreet nulla mi rhoncus risus. Ut dictum mauris nec ornare fermentum. Donec vulputate felis in vestibulum fermentum. Etiam varius varius nunc sed pharetra. In in est libero. Aenean auctor at ante eu scelerisque. Vestibulum vitae convallis mauris. Nam risus nibh, pellentesque a lacus eget, accumsan ultricies arcu. Phasellus finibus risus a velit dapibus, eget laoreet erat maximus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris varius volutpat luctus. Integer tincidunt feugiat justo. Phasellus eu sem urna. Pellentesque eget vehicula enim.
       </p>
-      <EmbededVideo youtubeId='cNr3r3nT8KE' />
+      <div style = {{width: '50%', marginLeft: 'auto', marginRight: 'auto'}}>
+        <EmbededVideo youtubeId='cNr3r3nT8KE' />
+      </div>
     </div>
   );
 }
