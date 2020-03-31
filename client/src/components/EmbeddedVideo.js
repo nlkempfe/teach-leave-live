df2f67e1657530f2b089b6daf70b474139472495
rieckn <35986321+rieckn@users.noreply.github.com>
2020-03-30 22:00:58 -0400

    Premium user can interact with premium video but standard user cant
    if a standard user is accessing a premium video a div is placed over the iframe making it noninteractive but if the user is a premium user the div is not placed allowing them to access the course

--- client/src/components/EmbeddedVideo.js
+++ client/src/components/EmbeddedVideo.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
+import YouTube from 'react-youtube';
+
+//import controller functions
+import {readUser} from '../firebase/controllers'
 
 
+//import firebase
+import {auth, provider, db} from '../firebase/firebaseInit';
 /*
 Provides an embeded youtube video that fills its container and matches an aspect ratio
 Provide props.aspectRatio to specify the aspect ratio (e.g. 16.0/9.0)
 Default aspect ratio is 16.0/9.0 if props.aspect ratio is not provided
 */
 function EmbeddedVideo (props) {
+  const [link, setLink] = useState('')
+  const [premium, setPremium] = useState(false)
+  const [userPremium, setUserPremium] = useState(false)
 
   /*Calculate inverse of aspect ratio and convert into a percentage*/
   const inverseAspectRatio = ((props.aspectRatio ? 1.0/props.aspectRatio : 9.0/16.0) * 100) + '%'
@@ -26,10 +35,37 @@ function EmbeddedVideo (props) {
     border: '0'
   };
 
+  const checkUser = () => {
+    console.log(!(premium))
+    if (userPremium || !(premium))
+    {
+      return (null)
+    }
+    else
+    {
+      return (<div style={iframe_style}></div>)
+    }
+  }
+
+  let userRef = db.collection('courses').doc(props.courseName);
+  let getDoc = userRef.get()
+  .then(doc => {
+    if (!doc.exists)
+    {
+      console.log('No Such Document')
+    }
+    else {
+        setLink(doc.data().link)
+        setPremium(doc.data().premium)
+        setUserPremium(readUser().premium)
+    }
+  })
+
   return (
     <div style = {props.style}>
       <div style = {container_style}>
-        <iframe style = {iframe_style} src={'https://www.youtube.com/embed/' + props.youtubeId} frameborder='0' allowfullscreen />
+        <iframe style = {iframe_style} src={'https://www.youtube.com/embed/' + link} frameborder='0' allowfullscreen />
+        {checkUser()}
       </div>
     </div>
   );
--- client/src/firebase/firebaseInit.js
+++ client/src/firebase/firebaseInit.js
@@ -4,14 +4,14 @@ import firebase from 'firebase';
 
 /* Import environment variables for initialization */
 const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDWipDGZ1j4xN632UILLy4QTD_SLDzSx7g",
+  authDomain: "teach-leave-live.firebaseapp.com",
+  databaseURL: "https://teach-leave-live.firebaseio.com",
+  projectId: "teach-leave-live",
+  storageBucket: "teach-leave-live.appspot.com",
+  messagingSenderId: "1083048857113",
+  appId: "1:1083048857113:web:54f27845f4e66ac65d2488",
+  measurementId: "G-9JCJ226ZD6"
 };
 
 firebase.initializeApp(config);
