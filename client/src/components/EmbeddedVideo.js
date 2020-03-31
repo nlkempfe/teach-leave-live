91835a736f7f33c613da54c850b598f42f41dea7
rieckn <35986321+rieckn@users.noreply.github.com>
2020-03-31 15:21:02 -0400

    Creates popup that allows premium user to select a course

--- client/src/components/EmbeddedVideo.js
+++ client/src/components/EmbeddedVideo.js
@@ -4,6 +4,13 @@ import YouTube from 'react-youtube';
 //import controller functions
 import {readUser} from '../firebase/controllers'
 
+//import material ui components
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 //import firebase
 import {auth, provider, db} from '../firebase/firebaseInit';
@@ -16,6 +23,10 @@ function EmbeddedVideo (props) {
   const [link, setLink] = useState('')
   const [premium, setPremium] = useState(false)
   const [userPremium, setUserPremium] = useState(false)
+  const [description, setDescription] = useState('')
+  const [name, setName] = useState('')
+  const [open, setOpen] = useState(null)
+  const [displayVideo, setDisplayVideo] = useState(false)
 
   /*Calculate inverse of aspect ratio and convert into a percentage*/
   const inverseAspectRatio = ((props.aspectRatio ? 1.0/props.aspectRatio : 9.0/16.0) * 100) + '%'
@@ -35,18 +46,6 @@ function EmbeddedVideo (props) {
     border: '0'
   };
 
-  const checkUser = () => {
-    console.log(!(premium))
-    if (userPremium || !(premium))
-    {
-      return (null)
-    }
-    else
-    {
-      return (<div style={iframe_style}></div>)
-    }
-  }
-
   let userRef = db.collection('courses').doc(props.courseName);
   let getDoc = userRef.get()
   .then(doc => {
@@ -58,15 +57,80 @@ function EmbeddedVideo (props) {
         setLink(doc.data().link)
         setPremium(doc.data().premium)
         setUserPremium(readUser().premium)
+        setDescription(doc.data().description)
+        if (premium)
+        {
+          setName(doc.data().name + ' (Premium)')
+        }
+        else 
+        {
+          setName(doc.data().name)
+        }
     }
   })
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const changeDisplay = () => {
+    setDisplayVideo(true);
+  }
+
+  //<iframe style={iframe_style} src={'https://www.youtube.com/embed/' + link + '/0.jpg'} frameborder='0' allowfullscreen />
+
+  const checkUser = () => {
+    //console.log(!(premium))
+    if ((userPremium || !(premium)) && !(displayVideo))
+    {
+      return (<div class='check'>
+                <Dialog
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="alert-dialog-slide-title"
+                  aria-describedby="alert-dialog-slide-description"
+                >
+                  <DialogTitle id="alert-dialog-slide-title">
+                    {name}
+                  </DialogTitle>
+                  <DialogContent>
+                    <DialogContentText id="alert-dialog-slide-description">
+                      {description}
+                    </DialogContentText>
+                  </DialogContent>
+                  <DialogActions>
+                    <Button onClick={changeDisplay} color="primary">
+                      Watch Video
+                    </Button>
+                    <Button onClick={handleClose} color="primary">
+                      Exit
+                    </Button>
+                  </DialogActions>
+                </Dialog>
+                <Button onClick={handleClickOpen}><img src={'https://img.youtube.com/vi/' + link + '/default.jpg'} /><br/>{name}</Button>
+              </div>)
+    }
+    else if (displayVideo)
+    {
+      return ( 
+          <div style={container_style}>
+            <iframe style={iframe_style} src={'https://www.youtube.com/embed/' + link} frameborder='0' allowFullScreen />
+          </div>
+      )
+    }
+    else
+    {
+      return (null)
+    }
+  }
+
   return (
-    <div style = {props.style}>
-      <div style = {container_style}>
-        <iframe style = {iframe_style} src={'https://www.youtube.com/embed/' + link} frameborder='0' allowfullscreen />
+    <div style={props.style}>
         {checkUser()}
-      </div>
     </div>
   );
 }
