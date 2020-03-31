2b44cb85f0132907d9d6499ab42a7cd0b30c72ed
Natascha Kempfe <nlkempfe@gmail.com>
2020-03-31 17:31:07 -0400

    Update AdminEvent.js and refactor event createion form to CreateEventDialog.js

--- client/src/views/AdminEvent.js
+++ client/src/views/AdminEvent.js
@@ -1,218 +1,164 @@
-import React, { useState } from 'react';
-import {debounce} from 'lodash';
-
-/* Import material-UI components */
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import DateFnsUtils from '@date-io/date-fns';
-import {
-  MuiPickersUtilsProvider,
-  KeyboardTimePicker,
-  KeyboardDatePicker,
-} from '@material-ui/pickers';
-
-/* Import custom components */
-import NavigationBar from '../components/NavigationBar.js';
+import React, { useEffect, useState } from 'react';
 
 /* Import firebase products */
 import {db} from '../firebase/firebaseInit';
 
-const useStyles = makeStyles(theme => ({
-    root: {
-      '& > *': {
-        margin: theme.spacing(1),
-      },
-    },
-  }));
+/* Import material-ui components */
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
 
-function AdminEvent() {
-    const classes = useStyles();
-    let currentDate = Date.now().toString();
-    /* Hook for the value of each form */
-    const [selectedDate, setSelectedDate] = useState(null);
-    const [selectedName, setSelectedName] = useState('');
-    const [selectedDescription, setSelectedDescription] = useState('');
-    const [selectedAddress, setSelectedAddress] = useState('');
-    const [selectedCity, setSelectedCity] = useState('');
-    const [selectedState, setSelectedState] = useState('');
-    const [selectedZip, setSelectedZip] = useState('');
 
-    //Splits description into an array of strings since firebase only allows strings of 99 characters or less
-    function makeDescriptionArray(eventDescription) {
-        let count = 0;
-        let i = 0;
-        for(i; i < Math.ceil(selectedDescription.length/99) - 1; i++)
-        {
-            eventDescription[i] = selectedDescription.substring(count, count + 98);
-            count += 99;
+/* Import material-ui icons */
+import AddIcon from '@material-ui/icons/Add';
+import CloseIcon from '@material-ui/icons/Close';
+import DeleteIcon from '@material-ui/icons/Delete';
+import DoneIcon from '@material-ui/icons/Done';
+import EditIcon from '@material-ui/icons/Edit';
+
+/* Import mui-datatables */
+import MUIDataTable from 'mui-datatables';
+
+function AdminEvent(props) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [rowIndex, setRowIndex] = useState(null);
+  const [events, setEvents] = useState([]);
+
+  const handleUpdate = () => {
+    db.collection('events').get().then(querySnapshot => {
+      let tempEvents = [];
+      querySnapshot.forEach(doc => {
+        let event = {
+            id: doc.data().id,
+            name: doc.data().name,
+            dateAndTime: doc.data().dateAndTime,
+            description: doc.data().description,
+            address: doc.data().address,
+            city: doc.data().city,
+            state: doc.data().city,
+            zip: doc.data().zip,
         }
-        console.log(i);
-        eventDescription[i] = selectedDescription.substring(count);
-    }
-
-    /* Handle changes of the value of each form using debounce to improve performance*/
-    const handleDateChange = date => {
-      setSelectedDate(date);
-    };
-    const handleNameChange = debounce ((name) => {
-      setSelectedName(name);
-    }, 500);
-    const handleDescriptionChange = debounce ((description) => {
-      setSelectedDescription(description);
-    }, 500);
-    const handleAddressChange = debounce ((address) => {
-      setSelectedAddress(address);
-    }, 500);
-    const handleCityChange = debounce ((city) => {
-      setSelectedCity(city);
-    }, 500);
-    const handleStateChange = debounce ((state) => {
-      setSelectedState(state);
-    }, 500);
-    const handleZipChange = debounce ((zip) => {
-      setSelectedZip(zip);
-    }, 500);
-
-    //Adds event to database after submission of form
-    const handleSubmit = async (event) => {
-      currentDate = selectedDate;
-      let eventDescription = new Array(Math.ceil(selectedDescription.length/99))
-      makeDescriptionArray(eventDescription)
-      let userDoc = db.collection('events').doc(selectedName);
-      let setUserDoc = await userDoc.set({
-          name : selectedName,
-          address : selectedAddress,
-          city : selectedCity,
-          state : selectedState,
-          zip : selectedZip,
-          description : eventDescription,
-          dateAndTime : currentDate
+        tempEvents.push(event);
       });
-    };
+      setEvents(tempEvents);
+    });
+  }
+
+  useEffect(() => {
+    handleUpdate();
+  }, []);
+
+  const handleAdd = () => {
+    /* TODO */
+  }
+  const handleDelete = (tableMeta) => {
+    /* TODO */
+  }
+  const handleEdit = (tableMeta) => {
+    setIsEditing(true);
+    setRowIndex(tableMeta.rowIndex);
+  }
+  const handleSubmit = (tableMeta) => {
+    setIsEditing(false);
+    /* TODO */
+  }
+
+  const columns = [
+    {
+      name: '',
+      label: '',
+      options: {
+        empty: true,
+        filter: false,
+        sort: false,
+        viewColumns: false
+      }
+    },
+    {
+      name: 'name',
+      label: 'Event Name',
+      options: {
+        customBodyRender: (value, tableMeta, updateValue) => {
+          return value;
+        },
+        filter: false,
+        sort: true
+      }
+    },
+    {
+        name: '',
+        options: {
+          customBodyRender: (value, tableMeta, updateValue) => {
+            if(isEditing) {
+              return (
+                <IconButton onClick = {event => handleSubmit(tableMeta)}>
+                  <DoneIcon/>
+                </IconButton>
+              );
+            } else {
+              return (
+                <IconButton onClick = {event => handleEdit(tableMeta)}>
+                  <EditIcon/>
+                </IconButton>
+              );
+            }
+          },
+          filter: false,
+          sort: false,
+          viewColumns: false
+        }
+      },
+      {
+        name: '',
+        label: '',
+        options: {
+          customBodyRender: (value, tableMeta, updateValue) => {
+            if(isEditing) {
+              return (
+                <IconButton onClick = {event => setIsEditing(false)}>
+                  <CloseIcon/>
+                </IconButton>
+              );
+            } else {
+              return (
+                <IconButton onClick = {event => handleDelete(tableMeta)}>
+                  <DeleteIcon/>
+                </IconButton>
+              );
+            }
+          },
+          filter: false,
+          sort: false,
+          viewColumns: false
+        }
+      },
+  ];
+  const options = {
+    customToolbar: () => {
+      return (
+        <IconButton onClick = {event => handleAdd()}>
+          <AddIcon/>
+        </IconButton>
+      );
+    },
+    disableToolbarSelect: true,
+    download: false,
+    elevation: 1,
+    filterType: 'checkbox',
+    print: false,
+    selectableRows: 'none',
+    selectableRowsHeader: false,
+  };
 
   return (
     <div>
-        <div>
-          <Grid container direction='column' justify='center' alignItems='center'>
-            <Grid item xs={4}></Grid>
-            <Grid item xs={4}>
-              <Grid container direction='row' justify='center' alignItems='flex-start'>
-                <h2>Create an Event</h2>
-              </Grid>
-                <form className={classes.root} onSubmit={handleSubmit}>
-                  <Grid container direction='row' justify='center' alignItems='flex-start' spacing={2}>
-                    <Grid item xs={12}>
-                     <TextField id='outlined-full-width'
-                                label='Event Name'
-                                fullWidth
-                                variant='outlined'
-                                required
-                                inputProps={{
-                                  maxLength: 99
-                                }}
-                                onChange={e => handleNameChange(e.target.value)}/>
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField id='outlined-multiline-static'
-                                 label='Event Description'
-                                 multiline
-                                 required
-                                 fullWidth
-                                 rows='4'
-                                 variant='outlined'
-                                 onChange={e => handleDescriptionChange(e.target.value)}/>
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField id='outlined-basic'
-                                 label='Address'
-                                 fullWidth variant='outlined'
-                                 required
-                                 inputProps={{
-                                  maxLength: 99
-                                 }}
-                                 onChange={e => handleAddressChange(e.target.value)}/>
-                    </Grid>
-                    <Grid item xs={4}>
-                      <TextField id='outlined-basic'
-                                 label='City'
-                                 fullWidth variant='outlined'
-                                 required
-                                 inputProps={{
-                                  maxLength: 99
-                                 }}
-                                 onChange={e => handleCityChange(e.target.value)}/>
-                    </Grid>
-                    <Grid item xs={4}>
-                      <TextField id='outlined-basic'
-                                 label='State'
-                                 fullWidth
-                                 variant='outlined'
-                                 required
-                                 inputProps={{
-                                  maxLength: 99
-                                 }}
-                                 onChange={e => handleStateChange(e.target.value)}/>
-                    </Grid>
-                    <Grid item xs={4}>
-                      <TextField id='outlined-basic'
-                                 label='Zip'
-                                 fullWidth variant='outlined'
-                                 required
-                                 inputProps={{
-                                  maxLength: 99
-                                 }}
-                                 onChange={e => handleZipChange(e.target.value)}/>
-                    </Grid>
-                    <Grid item xs={12}>
-                      <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                        <Grid container justify='center' spacing={2}>
-                          <Grid item xs={6}>
-                            <KeyboardDatePicker
-                              disableToolbar
-                              fullWidth
-                              required
-                              variant='inline'
-                              format='MM/dd/yyyy'
-                              margin='normal'
-                              id='date-picker-inline'
-                              label='Event Date'
-                              value={selectedDate}
-                              onChange={handleDateChange}
-                              KeyboardButtonProps={{
-                                'aria-label': 'change date',
-                              }}
-                            />
-                          </Grid>
-                          <Grid item xs={6}>
-                            <KeyboardTimePicker
-                              fullWidth
-                              required
-                              margin='normal'
-                              id='time-picker'
-                              label='Event Time'
-                              value={selectedDate}
-                              onChange={handleDateChange}
-                              KeyboardButtonProps={{
-                                'aria-label': 'change time',
-                              }}
-                            />
-                          </Grid>
-                        </Grid>
-                      </MuiPickersUtilsProvider>
-                      <Grid item>
-                        <Button variant='contained' color='primary' type='submit'>
-                          Submit
-                        </Button>
-                      </Grid>
-                  </Grid>
-                </Grid>
-              </form>
-            </Grid>
-          <Grid item xs={4}></Grid>
-        </Grid>
-      </div>
+      <Container fluid style = {{marginLeft: props.drawerWidth + 50, marginRight: 50, marginTop: 20}}>
+        <MUIDataTable title={'Manage Events'} data={events} columns={columns} options={options}/>
+      </Container>
     </div>
   );
 }
