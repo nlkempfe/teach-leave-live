4a12ace7b435549f658c65f9a91e5867a1146c9b
Dylan Booth <dbooth1@ufl.edu>
2020-04-11 16:50:52 -0400

    First draft of blog page

--- client/src/views/Blog.js
+++ client/src/views/Blog.js
@@ -1,20 +1,21 @@
 import React, { useState } from 'react';
-
+import {Avatar, Container, Card, CardContent, Typography, Grid, Button} from '@material-ui/core'
 import ProfileLink from '../components/ProfileLink.js'
-
 function Blog() {
   let userID = 'a2c60dovnNajfSG91rnB4UXmBlf2'
   return (
     <div>
+      <h1>Add a post</h1>
       <p>
-      This is the blog page.
+      Create a post to help your fellow teachers or just to express your concerns.
       </p>
-      <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dolor nibh, fermentum porttitor pulvinar sed, lacinia a lorem. Sed iaculis augue vel dictum rhoncus. Suspendisse venenatis, tellus nec sollicitudin molestie, urna felis semper mi, vitae laoreet nulla mi rhoncus risus. Ut dictum mauris nec ornare fermentum. Donec vulputate felis in vestibulum fermentum. Etiam varius varius nunc sed pharetra. In in est libero. Aenean auctor at ante eu scelerisque. Vestibulum vitae convallis mauris. Nam risus nibh, pellentesque a lacus eget, accumsan ultricies arcu. Phasellus finibus risus a velit dapibus, eget laoreet erat maximus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris varius volutpat luctus. Integer tincidunt feugiat justo. Phasellus eu sem urna. Pellentesque eget vehicula enim.
-      </p>
-      <div><ProfileLink uid={userID}/></div>
+     <Button variant="contained" color = "primary">+</Button>
+     <br/>
+     <br/>
+     <br/>
+     <h1>Discussions</h1>
     </div>
   );
 }
-
+//<div><ProfileLink uid={userID}/></div>
 export default Blog;
