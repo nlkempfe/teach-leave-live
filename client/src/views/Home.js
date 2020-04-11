ba1cb60901d5d17cfc03d33777aaa7a35c72c013
Dylan Booth <dbooth1@ufl.edu>
2020-04-11 16:46:32 -0400

    Home page with file

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,14 +1,27 @@
 import React, { useState } from 'react';
-
 /* Import assets */
-import logo from '../assets/logo.png'
-
+import logo from '../assets/logo.png';
+import classroom from "../assets/Class.png";
+import { CenturyView } from 'react-calendar';
 function Home(props) {
+  const styles = {
+    paperContainer: {
+        backgroundImage: `url(${classroom})`,
+        align:CenturyView,
+        height: 800,
+        backgroundRepeat: "no-Repeat"
+    }
+};
   return (
-    <div>
-    
+    <div style={styles.paperContainer}>
+      <h1 align = "center" style={{color:"white"}}>
+        Teach.Leave.Live
+      </h1>
+      <br/>
+      <p align = "center" style={{color:"white"}}>
+        We are the bridge between the classroom and personal lives of the teachers" 
+      </p>
     </div>
   );
 }
-
 export default Home;
