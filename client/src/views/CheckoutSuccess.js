cf33ab07407717d1a077e3e3928e0f6ef6980adb
widgeon1996 <33473305+widgeon1996@users.noreply.github.com>
2020-04-16 18:08:47 -0400

    Update CheckoutSuccess.js

--- client/src/views/CheckoutSuccess.js
+++ client/src/views/CheckoutSuccess.js
@@ -1,14 +1,22 @@
 import React from "react";
 import Axios from 'axios';
-
+import {readUser, updateUser} from "../firebase/controllers";
 export default class App extends React.Component {
   componenetDidMount(){
+
+    // import {readUser, updateUser} from "../firebase/controllers";
+// Read the user from the browser's localStorage
+let user = readUser();
+// Create an object representing the fields of the document you want to update
+let updatedValues = {premium: true};
+// Update the document with the function from controllers.js
+updateUser(user.uid, updatedValues)
     //
     /************** Post request to firebase. Changing user to from free to paid **************/
     //
-    //axios.post('/newSubscriber', {
+    // axios.post('/newSubscriber', {
     //  user: this.props.currUser
-    //})
+    // })
     setTimeout(this.props.history.push('/'), 5000)
   }
 
