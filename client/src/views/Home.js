0468bd703ff56e260eb3fbaecd6d148579d2d54b
Dylan Booth <dbooth1@ufl.edu>
2020-04-16 22:26:55 -0400

    deleted the white space,changed to a higher quality image

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,26 +1,40 @@
 import React, { useState } from 'react';
 /* Import assets */
 import logo from '../assets/logo.png';
-import classroom from "../assets/Class.png";
+import classroom from "../assets/c1.jpg";
 import { CenturyView } from 'react-calendar';
+
+import { createMuiTheme } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+
 function Home(props) {
   const styles = {
     paperContainer: {
-        backgroundImage: `url(${classroom})`,
-        align:CenturyView,
-        height: 800,
-        backgroundRepeat: "no-Repeat"
-    }
+      flexGrow:1,
+      backgroundImage: `url(${classroom})`,
+      align:CenturyView,
+      backgroundRepeat: "no-Repeat",
+      height: '100vh',
+      textAlign: 'center',
+      backgroundPosition: "center center",
+      backgroundSize: "cover",
+      backgroundAttachment: "fixed",
+  },
+    
 };
+
   return (
-    <div style={styles.paperContainer}>
-      <h1 align = "center" style={{color:"white"}}>
-        Teach.Leave.Live
-      </h1>
-      <br/>
-      <p align = "center" style={{color:"white"}}>
-        We are the bridge between the classroom and personal lives of the teachers" 
+    
+    <div style={styles.paperContainer} height = "max" width = "max">
+      
+      <Box bgcolor="text.disabled" color="background.paper" p={1}>
+        <h1>Teach. Leave. Live.</h1>
+          
+          <p align = "center">
+        "We are the bridge between the classroom and personal lives of the teachers" 
       </p>
+        </Box>
+        
     </div>
   );
 }
