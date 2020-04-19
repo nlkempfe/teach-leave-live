43b7b510179d5a326f887e90973666adbdad31c5
Natascha Kempfe <nlkempfe@gmail.com>
2020-04-19 18:53:04 -0400

    Update AdminBlog.js

--- client/src/views/AdminBlog.js
+++ client/src/views/AdminBlog.js
@@ -8,7 +8,6 @@ import UpdateBlogPostDialog from '../components/UpdateBlogPostDialog.js';
 import {db} from '../firebase/firebaseInit';
 
 /* Import material-ui components */
-import { createMuiTheme } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
@@ -17,63 +16,96 @@ import Select from '@material-ui/core/Select';
 import Table from '@material-ui/core/Table';
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 
 /* Import material-ui icons */
+import { createMuiTheme } from '@material-ui/core/styles';
 import AddIcon from '@material-ui/icons/Add';
 import CloseIcon from '@material-ui/icons/Close';
 import DeleteIcon from '@material-ui/icons/Delete';
 import DoneIcon from '@material-ui/icons/Done';
 import EditIcon from '@material-ui/icons/Edit';
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+
 
 /* Import mui-datatables */
 import MUIDataTable from 'mui-datatables';
-
-const theme = createMuiTheme({
-  overrides: {
-    MUIDataTableToolbarSelect: {
-      root: {
-        display: 'none'
-      }
-    }
-  }
-});
-
 
 function AdminBlog(props) {
 
   const [isEditing, setIsEditing] = useState(false);
   const [comments, setComments] = useState([]);
   const [posts, setPosts] = useState([]);
+  const [users, setUsers] = useState([]);
   const [open, setOpen] = useState(false);
   const [rowData, setRowData] = useState(null);
   const [width, setWidth] = useState({width: window.innerWidth});
+
+  const theme = createMuiTheme({
+     overrides: {
+       MUIDataTableSelectCell: {
+         expandDisabled: {
+           // Soft hide the button.
+           visibility: 'hidden',
+         },
+       },
+     },
+   });
 
   useEffect(() => {
     window.addEventListener('resize', setWidth(window.innerWidth));
   });
 
-  const handleUpdate = () => {
+  // Get collection of users to link user ids and names
+  const handleUpdateUsers = () => {
+    db.collection('users').get().then(querySnapshot => {
+      let tempUsers = [];
+      querySnapshot.forEach(doc => {
+        let user = {
+          id: doc.id,
+          name: doc.data().firstName + ' ' + doc.data().lastName,
+        }
+        tempUsers.push(user);
+      });
+      setUsers(tempUsers);
+    });
+  }
+
+  // Get collection of comments
+  const handleUpdateComments = () => {
     db.collection('comments').get().then(querySnapshot => {
       let tempComments = [];
       querySnapshot.forEach(doc => {
+        let name = '';
+        if(users.find(user => user.id == doc.data().userId)) {
+          name = users.find(user => user.id == doc.data().userId).name;
+        }
         let comment = {
           id: doc.id,
-          userId: doc.data().userId,
-          postId: doc.data().postId,
-          timestamp: doc.data().timestamp,
           content: doc.data().content,
+          postId: doc.data().postId,
+          userId: doc.data().userId,
+          timestamp: doc.data().timestamp,
           postTitle: '',
-          name: 'Name'
+          name: name,
         }
         tempComments.push(comment);
       });
       setComments(tempComments);
     });
+  }
 
+  const handleUpdate = () => {
+    // Get collection of posts
     db.collection('posts').get().then(querySnapshot => {
       let tempPosts = [];
       querySnapshot.forEach(doc => {
+        let tempComments = [];
+        comments.forEach(comment => {
+          if(comment.postId == doc.id) {
+            comment.postTitle = doc.data().title;
+            tempComments.push(comment);
+          }
+        });
         let post = {
             id: doc.id,
             allowCommenting: doc.data().allowCommenting,
@@ -82,8 +114,9 @@ function AdminBlog(props) {
             imageURL: doc.data().imageURL,
             timestamp: doc.data().timestamp,
             views: doc.data().views,
-            comments: [],
-            numComments: 0
+            comments: tempComments,
+            numComments: tempComments.length,
+            isExpandable: tempComments.length > 0 ? true : false,
         }
         tempPosts.push(post);
       });
@@ -92,22 +125,16 @@ function AdminBlog(props) {
   }
 
   useEffect(() => {
-    handleUpdate();
+    handleUpdateUsers();
   }, []);
 
   useEffect(() => {
-    posts.forEach(post => {
-      let tempComments = [];
-      comments.forEach(comment => {
-        if(comment.postId === post.id) {
-          comment.postTitle = post.title;
-          tempComments.push(comment);
-        }
-      });
-      post.comments = tempComments;
-      post.numComments = tempComments.length;
-    });
-  }, [posts, comments]);
+    handleUpdateComments();
+  }, [users]);
+
+  useEffect(() => {
+    handleUpdate();
+  }, [comments]);
 
   const handleClose = () => {
     setOpen(false);
@@ -116,11 +143,18 @@ function AdminBlog(props) {
   }
 
   const handleCommentDelete = (tableMeta) => {
-    console.log(comments[tableMeta.rowIndex].id);
+    db.collection('comments').doc(tableMeta.rowData[0]).delete();
+    handleUpdateComments();
   }
 
   const handleDelete = (tableMeta) => {
-    db.collection('posts').doc(posts[tableMeta.rowIndex].id).delete().then(handleUpdate());
+    db.collection('posts').doc(posts[tableMeta.rowIndex].id).delete();
+    db.collection('comments').where('postId', '==', posts[tableMeta.rowIndex].id).get().then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        doc.ref.delete();
+      });
+    });
+    handleUpdateComments();
   }
   const handleEdit = (tableMeta) => {
     setRowData(posts[tableMeta.rowIndex]);
@@ -271,9 +305,12 @@ function AdminBlog(props) {
     elevation: 1,
     expandableRows: true,
     isRowExpandable: (dataIndex, expandedRows) => {
-        // Prevent expand/collapse of any row if there are 4 rows expanded already (but allow those already expanded to be collapsed)
-        if (expandedRows.data.length > 0 && expandedRows.data.filter(d => d.dataIndex === dataIndex).length === 0) return false;
-        return true;
+        // Prevent expand/collapse of any row if there is a row expanded already (but allow those already expanded to be collapsed)
+        if(posts[dataIndex].isExpandable) {
+            if (expandedRows.data.length > 0 && expandedRows.data.filter(d => d.dataIndex === dataIndex).length === 0) return false;
+            return true;
+        }
+        return false;
       },
     filter: false,
     print: false,
@@ -282,9 +319,7 @@ function AdminBlog(props) {
       return (
         <TableRow>
           <TableCell colSpan = {11}>
-            <MuiThemeProvider theme={theme}>
-              <MUIDataTable data={posts[rowMeta.rowIndex].comments} columns={commentColumns} options={commentOptions}/>
-            </MuiThemeProvider>
+            <MUIDataTable data={posts[rowMeta.rowIndex].comments} columns={commentColumns} options={commentOptions}/>
           </TableCell>
         </TableRow>
       );
@@ -294,6 +329,16 @@ function AdminBlog(props) {
   };
 
   const commentColumns = [
+    {
+      name: 'id',
+      label: '',
+      options: {
+        display: false,
+        filter: false,
+        sort: false,
+        viewColumns: false
+      }
+    },
     {
       name: 'timestamp',
       label: 'Timestamp',
@@ -360,6 +405,7 @@ function AdminBlog(props) {
     elevation: 1,
     expandableRows: false,
     filter: false,
+    pagination: false,
     print: false,
     responsive: 'scrollMaxHeight',
     search: false,
@@ -368,11 +414,12 @@ function AdminBlog(props) {
     viewColumns: false,
   };
 
-
   return (
     <div>
       <Container fluid style = {{marginLeft: props.drawerWidth + 50, marginRight: 50, marginTop: 20, maxWidth: (width - props.drawerWidth - 100)}}>
-        <MUIDataTable title={'Manage Blog Posts'} data={posts} columns={columns} options={options}/>
+        <MuiThemeProvider theme={theme}>
+          <MUIDataTable title={'Manage Blog Posts'} data={posts} columns={columns} options={options}/>
+        </MuiThemeProvider>
         <CreateBlogPostDialog open = {open && !isEditing} handleClose = {post => handleClose()}/>
         <UpdateBlogPostDialog open = {open && isEditing} data = {rowData} handleClose = {event => handleClose()}/>
       </Container>
