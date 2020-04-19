80ccee301bff69fd7e3a9fda80e33aca6e1b15bc
Natascha Kempfe <nlkempfe@gmail.com>
2020-04-19 16:45:50 -0400

    Update AdminBlog.js

--- client/src/views/AdminBlog.js
+++ client/src/views/AdminBlog.js
@@ -8,13 +8,16 @@ import UpdateBlogPostDialog from '../components/UpdateBlogPostDialog.js';
 import {db} from '../firebase/firebaseInit';
 
 /* Import material-ui components */
+import { createMuiTheme } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
+import Table from '@material-ui/core/Table';
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 
 /* Import material-ui icons */
 import AddIcon from '@material-ui/icons/Add';
@@ -26,8 +29,21 @@ import EditIcon from '@material-ui/icons/Edit';
 /* Import mui-datatables */
 import MUIDataTable from 'mui-datatables';
 
+const theme = createMuiTheme({
+  overrides: {
+    MUIDataTableToolbarSelect: {
+      root: {
+        display: 'none'
+      }
+    }
+  }
+});
+
+
 function AdminBlog(props) {
+
   const [isEditing, setIsEditing] = useState(false);
+  const [comments, setComments] = useState([]);
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
   const [rowData, setRowData] = useState(null);
@@ -38,6 +54,23 @@ function AdminBlog(props) {
   });
 
   const handleUpdate = () => {
+    db.collection('comments').get().then(querySnapshot => {
+      let tempComments = [];
+      querySnapshot.forEach(doc => {
+        let comment = {
+          id: doc.id,
+          userId: doc.data().userId,
+          postId: doc.data().postId,
+          timestamp: doc.data().timestamp,
+          content: doc.data().content,
+          postTitle: '',
+          name: 'Name'
+        }
+        tempComments.push(comment);
+      });
+      setComments(tempComments);
+    });
+
     db.collection('posts').get().then(querySnapshot => {
       let tempPosts = [];
       querySnapshot.forEach(doc => {
@@ -49,22 +82,41 @@ function AdminBlog(props) {
             imageURL: doc.data().imageURL,
             timestamp: doc.data().timestamp,
             views: doc.data().views,
+            comments: [],
+            numComments: 0
         }
         tempPosts.push(post);
       });
       setPosts(tempPosts);
     });
-    console.log('posts', posts);
   }
 
   useEffect(() => {
     handleUpdate();
   }, []);
 
+  useEffect(() => {
+    posts.forEach(post => {
+      let tempComments = [];
+      comments.forEach(comment => {
+        if(comment.postId === post.id) {
+          comment.postTitle = post.title;
+          tempComments.push(comment);
+        }
+      });
+      post.comments = tempComments;
+      post.numComments = tempComments.length;
+    });
+  }, [posts, comments]);
+
   const handleClose = () => {
     setOpen(false);
     setIsEditing(false);
     handleUpdate();
+  }
+
+  const handleCommentDelete = (tableMeta) => {
+    console.log(comments[tableMeta.rowIndex].id);
   }
 
   const handleDelete = (tableMeta) => {
@@ -167,6 +219,14 @@ function AdminBlog(props) {
       }
     },
     {
+      name: 'numComments',
+      label: 'Comments',
+      options: {
+        filter: false,
+        sort: true
+      }
+    },
+    {
         name: '',
         options: {
           customBodyRender: (value, tableMeta, updateValue) => {
@@ -219,19 +279,95 @@ function AdminBlog(props) {
     print: false,
     responsive: 'scrollMaxHeight',
     renderExpandableRow: (rowData, rowMeta) => {
-      const colSpan = rowData.length + 1;
       return (
         <TableRow>
-          <TableCell colSpan={colSpan}>
-            Custom expandable row option.
+          <TableCell colSpan = {11}>
+            <MuiThemeProvider theme={theme}>
+              <MUIDataTable data={posts[rowMeta.rowIndex].comments} columns={commentColumns} options={commentOptions}/>
+            </MuiThemeProvider>
           </TableCell>
         </TableRow>
       );
     },
-    onRowsExpand: (curExpanded, allExpanded) => console.log(curExpanded, allExpanded),
     selectableRows: 'none',
     selectableRowsHeader: false,
   };
+
+  const commentColumns = [
+    {
+      name: 'timestamp',
+      label: 'Timestamp',
+      options: {
+        customBodyRender: (value, tableMeta, updateValue) => {
+          const d = value.toDate();
+          const date = ("0"+(d.getMonth()+1)).slice(-2)  + "/" + ("0" + d.getDate()).slice(-2) + "/" + d.getFullYear() + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2)
+          return (date);
+        },
+        filter: false,
+        sort: true
+      }
+    },
+    {
+      name: 'name',
+      label: 'Name',
+      options: {
+        sort: true
+      }
+    },
+    {
+      name: 'postTitle',
+      label: 'Blog Post Title',
+      options: {
+        sort: true
+      }
+    },
+    {
+      name: 'content',
+      label: 'Content',
+      options: {
+        customBodyRender: (value, tableMeta, updateValue) => {
+          if(value.length > 30) {
+            return (value.substring(0, 30) + '...');
+          } else {
+            return (value);
+          }
+        },
+        filter: false,
+        sort: true
+      }
+    },
+      {
+        name: '',
+        label: '',
+        options: {
+          customBodyRender: (value, tableMeta, updateValue) => {
+            return (
+              <IconButton onClick = {post => handleCommentDelete(tableMeta)}>
+                <DeleteIcon/>
+              </IconButton>
+            );
+          },
+          filter: false,
+          sort: false,
+          viewColumns: false
+        }
+      },
+  ];
+
+  const commentOptions = {
+    disableToolbarSelect: true,
+    download: false,
+    elevation: 1,
+    expandableRows: false,
+    filter: false,
+    print: false,
+    responsive: 'scrollMaxHeight',
+    search: false,
+    selectableRows: 'none',
+    selectableRowsHeader: false,
+    viewColumns: false,
+  };
+
 
   return (
     <div>
