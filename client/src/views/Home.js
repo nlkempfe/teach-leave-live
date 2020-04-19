a56be9b0fc2be84f6a67beed7e97ea41ae4f0814
Dylan Booth <dbooth1@ufl.edu>
2020-04-18 22:50:50 -0400

    Implemented material ui typography and made banner span entire background image

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 /* Import assets */
+
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+
 import logo from '../assets/logo.png';
 import classroom from "../assets/c1.jpg";
-import { CenturyView } from 'react-calendar';
 
+import { CenturyView } from 'react-calendar';
 import { createMuiTheme } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
+
 
 function Home(props) {
   const styles = {
@@ -20,21 +24,30 @@ function Home(props) {
       backgroundSize: "cover",
       backgroundAttachment: "fixed",
   },
-    
+  paperContainer2: {
+    flexGrow:1,
+    height: '83vh',
+},
 };
 
   return (
     
     <div style={styles.paperContainer} height = "max" width = "max">
       
-      <Box bgcolor="text.disabled" color="background.paper" p={1}>
-        <h1>Teach. Leave. Live.</h1>
-          
-          <p align = "center">
+      <Box bgcolor="text.disabled" color="background.paper" p={3}>
+
+      <Typography variant="h4" component="h2" gutterBottom>
+        Teach. Leave. Live.
+      </Typography>
+
+      <Typography variant="subtitle1" gutterBottom>
         "We are the bridge between the classroom and personal lives of the teachers" 
-      </p>
+      </Typography>
         </Box>
         
+        <Box bgcolor="text.disabled" color="background.paper" p={1} style={styles.paperContainer2}>
+          
+        </Box>
     </div>
   );
 }
