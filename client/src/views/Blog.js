bf9598e8f5485dd41562adfab536f7785323871b
Dylan Booth <dbooth1@ufl.edu>
2020-04-18 23:03:50 -0400

    test

--- client/src/views/Blog.js
+++ client/src/views/Blog.js
@@ -1,20 +1,87 @@
 import React, { useState } from 'react';
-
+import {Avatar, Container, Card, CardContent, Typography, Grid, Button} from '@material-ui/core'
 import ProfileLink from '../components/ProfileLink.js'
+import logo from '../assets/logo.png';
+import image from '../assets/BlogImg.png';
+import { CenturyView } from 'react-calendar';
+import { createMuiTheme } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardMedia from '@material-ui/core/CardMedia';
+import TextField from '@material-ui/core/TextField';
 
 function Blog() {
-  let userID = 'a2c60dovnNajfSG91rnB4UXmBlf2'
+  const styles = {
+    paperContainer: {
+        flexGrow:1,
+        backgroundImage: `url(${image})`,
+        height : 220,
+        backgroundRepeat: "no-Repeat"
+    },
+    media: {
+      flexGrow: 1,
+      width: '25vh',
+      height: '25vh',
+      paddingTop: 'auto', // 16:9,
+      marginTop:'10'
+    }
+};
+  //let userID = 'a2c60dovnNajfSG91rnB4UXmBlf2'
   return (
     <div>
+      <Grid container spacing={0} justify='flex-start'>
+      <Grid item xs={6} sm={3}>
+      <Box style={styles.paperContainer} bgcolor="text.disabled" color="background.paper">
+        </Box>
+      </Grid>
+      <Grid item xs={12} sm={9}>
+      <Box bgcolor="text.disabled" color="background.paper" p={8.9} align = "center">
+      <h1> Blog Forum</h1>
+       </Box> 
+      </Grid>
+      </Grid>
+      <br/>
+      <br/>
+      <h1>Add a post</h1>
       <p>
-      This is the blog page.
+      Create a post to help your fellow teachers or just to express your concerns.
       </p>
-      <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dolor nibh, fermentum porttitor pulvinar sed, lacinia a lorem. Sed iaculis augue vel dictum rhoncus. Suspendisse venenatis, tellus nec sollicitudin molestie, urna felis semper mi, vitae laoreet nulla mi rhoncus risus. Ut dictum mauris nec ornare fermentum. Donec vulputate felis in vestibulum fermentum. Etiam varius varius nunc sed pharetra. In in est libero. Aenean auctor at ante eu scelerisque. Vestibulum vitae convallis mauris. Nam risus nibh, pellentesque a lacus eget, accumsan ultricies arcu. Phasellus finibus risus a velit dapibus, eget laoreet erat maximus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris varius volutpat luctus. Integer tincidunt feugiat justo. Phasellus eu sem urna. Pellentesque eget vehicula enim.
-      </p>
-      <div><ProfileLink uid={userID}/></div>
+     <Button variant="contained" color = "primary">+</Button>
+     <br/>
+     <br/>
+     <br/>
+     <h1>Discussions</h1>
+     <form noValidate autoComplete="off">
+        <TextField id="outlined-basic" label="Search for discussion" variant="outlined" />
+    </form>
+    <br/>
+     <Card>
+       <CardActionArea>
+       <CardMedia
+          image={require('../assets/logo.png')}
+          title="Contemplative Reptile"
+          style={styles.media}
+          height="0"
+      />
+           <CardContent>
+             <Typography gutterBottom variant="h5" component="h2">
+            Teach.Leave.Live
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+            TEST
+          </Typography>
+           </CardContent>
+         </CardActionArea>
+         <CardActions>
+         <Button size="small" color="primary">
+          See More
+        </Button>
+           </CardActions>
+       </Card>
     </div>
   );
 }
-
+//<div><ProfileLink uid={userID}/></div>
 export default Blog;
