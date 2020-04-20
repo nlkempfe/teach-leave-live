7b82b59254537519878784dc5788716ed34a4d4e
Natascha Kempfe <nlkempfe@gmail.com>
2020-04-20 00:01:58 -0400

    Update Home.js

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -7,7 +7,14 @@ import classroom from '../assets/classroom.jpg';
 /* Import material-ui components */
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
+
+/* Import material-ui icons */
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TwitterIcon from '@material-ui/icons/Twitter';
+
 
 /* Import custom components */
 import NewsletterSubscribeButton from '../components/NewsletterSubscribeButton';
@@ -19,6 +26,9 @@ function Home(props) {
       <Container fluid style = {{paddingTop: '10%'}}>
         <Typography variant= 'h1' gutterBottom >Teach. Leave. Live.</Typography>
         <Typography variant= 'subtitle1' color = 'primary.contrastText' gutterBottom >We are the bridge between the classroom and personal lives of the teachers.</Typography>
+        <Link href="https://www.instagram.com/teachleavelive/" target="_blank" color = 'inherit'><InstagramIcon /> </Link>
+        <Link href="#" target="_blank" color = 'inherit'><FacebookIcon /></Link>
+        <Link href="#" target="_blank" color = 'inherit'><TwitterIcon /></Link>
         <NewsletterSubscribeButton open = {true}/>
       </Container>
     </div>
